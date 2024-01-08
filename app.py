b43ad1ffcd5ab99ce13b167858ba69b28dd33b11
thegreytangent <thegreytangent@gmail.com>
2024-01-08 19:32:23 +0800

    Feat: Working on formattin packets

--- app.py
+++ app.py
@@ -26,6 +26,20 @@ def get_mac_addr(addr) :
     bytes_str = map('{:02x}'.format, addr)
     return ':'.join(bytes_str).upper()
 
+
+#Get IPv4 
+def ipv4_packet(data):
+    version_header_length = data[0]
+    version = version_header_length >> 4
+    header_length = (version_header_length & 15) * 4
+    ttl, proto, src, target = struct.unpack('! 8x B B 2x 4s 4s', data[:20])
+    return version, header_length, ttl, proto, ipv4(src), ipv4(target), data[header_length:]
+
+def ipv4(addr):
+    return '.'.join(map(str, addr))
+
+
+
 main()
      
 
