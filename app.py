b68bf0a56b6ba6950736fda0005da0041ff21126
thegreytangent <thegreytangent@gmail.com>
2024-01-08 18:04:48 +0800

    Feat: Setup internet frame funcfunction

---
+++ app.py
@@ -0,0 +1,13 @@
+import socket
+import struct
+import textwrap
+
+
+def ethernet_frame(data):
+    dest_mac, src_mac, proto = struct.unpack('! 6s 6s H', data[:14])
+    return get_mac_addr(dest_mac), get_mac_addr(src_mac), socket.htons(proto), data[:14]
+
+def get_mac_addr(mac) :
+    pass
+     
+
